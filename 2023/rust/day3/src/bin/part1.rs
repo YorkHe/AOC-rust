86612d69140a2d2ad50e101b5d52a0aaf3ad2bde
York He <york.heyu@gmail.com>
2023-12-03 12:34:27 -0800

    AOC 2023 Day3 in rust

---
+++ 2023/rust/day3/src/bin/part1.rs
@@ -0,0 +1,104 @@
+use std::{
+    collections::{BTreeMap, HashSet},
+    io::Result,
+};
+
+fn main() {
+    let input = include_str!("../../../../input/day3/input.txt");
+
+    let result = part1(input).unwrap();
+    println!("Result: {}", result);
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct Part {
+    part_num: usize,
+    x_start: usize,
+    x_end: usize,
+    y: usize,
+}
+
+fn update_parts_map(num: u32, i: usize, j: usize, parts_map: &mut BTreeMap<usize, Vec<Part>>) {
+    let part = Part {
+        part_num: num as usize,
+        x_start: (j - num.to_string().len()),
+        x_end: j - 1,
+        y: i,
+    };
+
+    if parts_map.contains_key(&part.y) {
+        parts_map.get_mut(&part.y).unwrap().push(part);
+    } else {
+        parts_map.insert(part.y, vec![part]);
+    }
+}
+
+fn part1(input: &str) -> Result<u32> {
+    let map = input
+        .lines()
+        .map(|line| line.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let mut parts_map: BTreeMap<usize, Vec<Part>> = BTreeMap::new();
+    let mut symbols: Vec<(usize, usize)> = vec![];
+
+    for i in 0..map.len() {
+        let mut num = 0;
+        for j in 0..map[i].len() {
+            if map[i][j].is_digit(10) {
+                num = num * 10 + map[i][j].to_digit(10).unwrap();
+            } else {
+                if num != 0 {
+                    update_parts_map(num, i, j, &mut parts_map);
+                    num = 0;
+                }
+
+                if map[i][j] != '.' {
+                    symbols.push((j, i));
+                }
+            }
+        }
+
+        if num != 0 {
+            update_parts_map(num, i, map[i].len(), &mut parts_map);
+        }
+    }
+
+    let mut part_set: HashSet<&Part> = HashSet::new();
+
+    for symbol in symbols {
+        for offset in -1..=1 {
+            let x: i32 = symbol.0 as i32;
+            let y: i32 = symbol.1 as i32 + offset;
+
+            if x < 0 || y < 0 {
+                continue;
+            }
+
+            if let Some(parts) = parts_map.get(&(y as usize)) {
+                for part in parts {
+                    if part.x_start as i32 - 1 == x
+                        || part.x_end as i32 + 1 == x
+                        || (part.x_start as i32 <= x && part.x_end as i32 >= x)
+                    {
+                        part_set.insert(part);
+                    }
+                }
+            }
+        }
+    }
+
+    let sum = part_set.iter().fold(0, |acc, part| acc + part.part_num);
+
+    Ok(sum as u32)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_part1() {
+        let input = include_str!("../../../../input/day3/example.txt");
+        let result = super::part1(input).unwrap();
+        assert_eq!(result, 4361);
+    }
+}
